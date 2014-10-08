89ddd399e502626d2086cfb943f2c9632aa4b6ab
Le Petit Chat <wizarw@gmail.com>
2014-10-08 17:59:44 +0200

    Je vien de crée mes class Connardo !

---
+++ Pangolin/src/in/DB.java
@@ -0,0 +1,12 @@
+package in;
+
+public class DB 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("DB pangol");
+
+	}
+
+}
---
+++ Pangolin/src/pangol/Main.java
@@ -0,0 +1,12 @@
+package pangol;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("in Main");
+
+	}
+
+}
